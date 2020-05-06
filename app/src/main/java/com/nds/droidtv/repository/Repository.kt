06b4dabed05a1b99162da10e6e3019c6774eac05
MikeMayfield <mike@themayfields.com>
2020-05-06 07:02:02 -0700

    Changes from DroidTV7 version of project.

--- app/src/main/java/com/nds/droidtv/repository/Repository.kt
+++ app/src/main/java/com/nds/droidtv/repository/Repository.kt
@@ -1,8 +1,7 @@
 package com.nds.droidtv.repository
 
 import android.content.Context
-import com.nds.droidtv.models.DeviceContext
-import com.nds.droidtv.models.SearchContext
+import com.nds.droidtv.models.*
 
 
 /**
@@ -15,6 +14,41 @@ object Repository {
     val searchContext = SearchContext()  //Current search context
     val deviceContext = DeviceContext()  //Current device hardware context
 
+    /**
+     * Find a series by ID
+     *
+     * @param seriesId ID of the series to find
+     */
+    fun findSeries(seriesId: Int) : Series {
+        return Series(1, "Series not found")  //TODO: Implement
+    }
+
+    /**
+     * Update series information for changes to one or more properties
+     *
+     * @param series Series instance with changes
+     */
+    fun updateSeries(series: Series) {
+        //TODO: Implement
+    }
+
+    /**
+     * Find an Episode by ID
+     *
+     * @param episodeId ID of the series to find
+     */
+    fun findEpisode(episodeId: Int) : Episode {
+        return Episode(1, 1, 0, 0, title = "Episode not found")  //TODO: Implement
+    }
+
+    /**
+     * Update episode information for changes to one or more properties
+     *
+     * @param episode Episode instance with changes
+     */
+    fun updateEpisode(episode: Episode) {
+        //TODO: Implement
+    }
 
 //    /**
 //     * Override invoke operator to support singleton constructor with parameter
